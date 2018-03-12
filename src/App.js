701b7f0e76b7788ae58f0f5f9e93f15588acbc67
Mike Campbell <mikecampbell@Mikes-MacBook-Pro.local>
2018-03-11 23:42:39 -0400

    added large image functionality + modals

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import './App.scss'
 import mount from './AppContainer'
 import {s3Buckets} from './constants/s3Buckets'
-import {Row} from 'react'
+import {Modal} from 'react-bootstrap'
 export class App extends Component {
   constructor(props){
     super(props)
     this.renderThumbnails = this.renderThumbnails.bind(this)
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+    this.renderModal = this.renderModal.bind(this)
+    this.state = {
+      curImage: null,
+      showModal: false
+    }
   }
   componentDidMount() {
     this.props.getThumbnails()
@@ -16,9 +23,10 @@ export class App extends Component {
     return (
       <div className="App">
         <h1 className="App-intro">Welcome!</h1>
-        <p>This site is built and maintained by Mike Campbell, and is being used as a photo repository for him and his family. If you would like access full to it, please log in or contact him.</p>
+        <p>This site is built and maintained by Mike Campbell, and is being used as a photo repository for him and his family. If you would like access full to it, please log in or contact him. [UNDER CONSTRUCTION]</p>
         <div className="welcomeImages">
           {this.renderThumbnails()}
+          {this.renderModal()}
         </div>
       </div>
     )
@@ -28,12 +36,38 @@ export class App extends Component {
     if (!!this.props.thumbKeys) {
       return this.props.thumbKeys.map((imgKey, index) => {
         return (
-          <img className="thumbnailImage" key={index}
+          <img onClick={this.openModal} className="thumbnailImage" key={index}
             src={s3Buckets.thumbnailBucket + imgKey} id={imgKey} alt="downloaded thumbnail"/>
         )
       })
     }
   }
+
+  renderModal() {
+        return (
+            <Modal animation={false} backdrop={true} bsSize="lg" show={this.state.showModal} onHide={this.closeModal}>
+                <img src={s3Buckets.imageBucket + this.state.curImage} width="100%" alt="full version"/>
+            </Modal>
+        )
+    }
+
+    openModal(e) {
+      console.log('e', e.target)
+
+        this.setState({
+            ...this.state,
+            curImage: e.target.id,
+            showModal: true
+        })
+    }
+
+    closeModal() {
+        this.setState({
+            ...this.state,
+            curImage: null,
+            showModal: false
+        })
+    }
 }
 
 export default mount(App)
